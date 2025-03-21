1967a70b6c5ae9a6043c60920a434c16f4c8c596
nazar <kritsaknazar@gmail.com>
2025-03-21 22:51:24 +0200

    - made a script "Counter"   - class Counter   - def __init__ count=0   - def increment / додає +1   - def decrement / віднімає -1   - def reset / ресет на нуль   - def info / рахунок

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('test')
+class Counter:
+    def __init__(self, count=0):
+        self.count = count
+
+    def increment(self):
+        self.count += 1
+        print(f"додав +1: {self.count}")
+
+    def decrement(self):
+        self.count -= 1
+        print(f"відняв -1: {self.count}")
+
+    def reset(self):
+        self.count = 0
+        print(f"ресет: {self.count}")
+
+    def info(self):
+        print(f"рахунок: {self.count}")
+
+
+counter = Counter()
+counter.info()
+counter.increment()
+counter.decrement()
+counter.reset()
+counter.info()
